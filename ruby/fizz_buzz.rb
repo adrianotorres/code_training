ffb6102bcf5f0e46baf1733b526620b1f2cac1aa
Adriano Skilo <adriano.skilo@gmail.com>
2018-08-28 18:31:37 -0300

    fizzbuzz fixed

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,9 +1,8 @@
 def fizz_buzz(n)
-    (1..n).each do |i|
-        result = 'fizz' if i % 2 === 0
-        result = "#{result}buzz" if i % 3 === 0
-        puts result || i
-    end
+  (1..n).each do |i|
+    result = 'fizz' if (i % 2).eql?(0)
+    result = "#{result}buzz" if (i % 3).eql?(0)
+    puts result || i
+  end
 end
 fizz_buzz(ARGV[0].to_i)
-
